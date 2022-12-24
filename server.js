973b90a23355966440ba9ed10313efb208de338e
ramesh banakar <ramesh.s.b@campusuvce.in>
2022-12-24 23:37:44 +0530

    concurrency and nodemon setup

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const express=require("express")
+const express = require("express");
+const app = express();
+app.use(express.json());
+app
+  .get("/", (req, res) => {
+    res.send("hello");
+  })
+  .listen(5000, () => console.log("server started@localhost 5000"));
